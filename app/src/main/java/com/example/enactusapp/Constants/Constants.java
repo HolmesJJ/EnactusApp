dacb24d51a334ba7b941b08b368b35987043d9a2
holmesjj <hujiajun19961024@gmail.com>
2020-09-18 17:00:14 +0800

    Update AWS IP address

--- app/src/main/java/com/example/enactusapp/Constants/Constants.java
+++ app/src/main/java/com/example/enactusapp/Constants/Constants.java
@@ -2,7 +2,7 @@ package com.example.enactusapp.Constants;
 
 public class Constants {
 
-    public static final String IP_ADDRESS = "http://192.168.1.186/";
+    public static final String IP_ADDRESS = "http://13.212.39.94/";
     public static final String FIREBASE_ADDRESS = "https://fcm.googleapis.com/fcm/send";
     public static final String SERVER_KEY = "key=" + "AAAATXQDbI0:APA91bH_feXkLVx3JfZOdEJ2azTPegMuRcpd65NOV2MGRCBsixeREJ5beX5BZzueNxssd8839iEL71OR-oZiJVT_b-mYeymqBXdJfy85UicC9kE2nYqyD8Fbj2I6HmGGGls07r5UAo4q";
 
