36522be40de0280777b8597e6a5789ae5ba8623b
holmesjj <hujiajun19961024@gmail.com>
2021-01-17 17:14:59 +0800

    Update Settings

--- app/src/main/java/com/example/enactusapp/Constants/Constants.java
+++ app/src/main/java/com/example/enactusapp/Constants/Constants.java
@@ -2,9 +2,9 @@ package com.example.enactusapp.Constants;
 
 public class Constants {
 
-    public static final String IP_ADDRESS = "http://13.212.39.94/";
+    public static final String IP_ADDRESS = "http://192.168.1.156/";
     public static final String FIREBASE_ADDRESS = "https://fcm.googleapis.com/fcm/send";
-    public static final String SERVER_KEY = "key=" + "AAAATXQDbI0:APA91bH_feXkLVx3JfZOdEJ2azTPegMuRcpd65NOV2MGRCBsixeREJ5beX5BZzueNxssd8839iEL71OR-oZiJVT_b-mYeymqBXdJfy85UicC9kE2nYqyD8Fbj2I6HmGGGls07r5UAo4q";
+    public static final String SERVER_KEY = "key=" + "AAAApCBXmvo:APA91bE9AqJEgc3vlpLru-Zh53gQec5T8XtTHxBhKBihFFWIgdrPpAjvk6R5AGCGeTGKsbYvw8r2b0wUtjNb2y0FcE-gh5KCzxh_c-O638BKyGi29ek74WjIYQq6tBECJRB2OYuq50ca";
 
     // GAZE密钥
     public static final String GAZE_LICENSE_KEY = "dev_4wgm4yzqvyk2gdnofwonumr2zwxci8ntw1m6mqyh";
